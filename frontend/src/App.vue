22f16140cbbbe511e4e0543b212f803ad7a30ca2
lopliok <lopliok@gmail.com>
2025-04-14 02:06:33 +0200

    fe: add evn var

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -90,7 +90,7 @@
 import { ref, computed, toRaw } from "vue";
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3000/api";
 
 export default {
 
