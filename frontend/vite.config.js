b3e1053007617068b50c349bfb396610c96f698e
lopliok <lopliok@gmail.com>
2025-04-14 02:24:13 +0200

    fe: build setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
 export default defineConfig({
   plugins: [vue()],
+  base: './', // 🔥 DŮLEŽITÉ pro deployment na NGINX
+  build: {
+    outDir: 'dist' // defaultně je to tak, ale pro jistotu
+  },
   server: {
     port: 5173
   }
